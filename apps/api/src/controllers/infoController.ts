f0a5350df79ac782a641fdfe349ec5e807c187cf
Emran Widad <emranwidad91@gmail.com>
2026-02-02 17:46:46 +0300

    Add logger functionality winston

--- apps/api/src/controllers/infoController.ts
+++ apps/api/src/controllers/infoController.ts
@@ -1,7 +1,10 @@
 import { type Request, type Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import logger from "./../logs/logger.js";
+
 
 const getInfoData = (req: Request, res: Response) => {
+    logger.info("Health check endpoint was hit");
     return res.status(StatusCodes.OK).json({
         success: true,
         message: "Your Api is alive",
